1bf79a551b65eb9bfdbd5fdb7a0ade77b96bc4a3
Michael Sumner <mdsumner@gmail.com>
2019-11-13 21:48:44 +0000

    tidy

--- data-raw/tasmap_layers.R
+++ data-raw/tasmap_layers.R
@@ -1,15 +1,3 @@
-d <- structure(list(Name = c("Orthophoto", "Tasmap25k", "Tasmap100k",
-                        "Tasmap250k", "TasmapRaster", "Hillshade", "TopoGrey", "Topographic"
-), ServiceUrl = c(
-                  "https://services.thelist.tas.gov.au/arcgis/services/Basemaps/Orthophoto/MapServer/WMSServer?",
-                  "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/Tasmap25K/ImageServer/WMSServer?",
-                  "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/Tasmap100K/ImageServer/WMSServer?",
-                  "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/Tasmap250K/ImageServer/WMSServer?",
-                  "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/TasmapRaster/ImageServer/WMSServer?",
-                  "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/Hillshade/MapServer/WMSServer?",
-                  "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/TopographicGrayScale/ImageServer/WMSServer?",
-                  "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/Topographic/ImageServer/WMSServer?"
-)), class = "data.frame", row.names = c(NA, -8L))
 
 
 
@@ -23,5 +11,21 @@ tasmap_layers <- tibble::tribble(
   "Tasmap250K", "https://services.thelist.tas.gov.au/arcgis/services/Basemaps/Tasmap250K/MapServer/WMSServer?",
   "Topographic","https://services.thelist.tas.gov.au/arcgis/services/Basemaps/Topographic/MapServer/WMSServer?"
 )
+
 ##tasmap_layers <- dplyr::bind_rows(d, tasmap_layers)
 usethis::use_data(tasmap_layers, overwrite = TRUE)
+
+
+## old way
+# d <- structure(list(Name = c("Orthophoto", "Tasmap25k", "Tasmap100k",
+#                         "Tasmap250k", "TasmapRaster", "Hillshade", "TopoGrey", "Topographic"
+# ), ServiceUrl = c(
+#                   "https://services.thelist.tas.gov.au/arcgis/services/Basemaps/Orthophoto/MapServer/WMSServer?",
+#                   "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/Tasmap25K/ImageServer/WMSServer?",
+#                   "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/Tasmap100K/ImageServer/WMSServer?",
+#                   "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/Tasmap250K/ImageServer/WMSServer?",
+#                   "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/TasmapRaster/ImageServer/WMSServer?",
+#                   "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/Hillshade/MapServer/WMSServer?",
+#                   "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/TopographicGrayScale/ImageServer/WMSServer?",
+#                   "http://services.thelist.tas.gov.au/arcgis/services/Basemaps/Topographic/ImageServer/WMSServer?"
+# )), class = "data.frame", row.names = c(NA, -8L))
